fd46dd7fb766d022a5bf44264f778a4e7250192e
昊钰 王 <wanghaoyu666@bupt.edu.cn>
2022-10-01 00:07:27 +0800

    1211

--- test1/determine.cpp
+++ test1/determine.cpp
@@ -12,6 +12,7 @@ bool determine(int n, int x, int y)
     int count, pre, cur;
     cur = 0;
     pre = n - 1;
+    if (x>n) return false;
     for (int i = 0; i < n; i++)
     {
         if (i != n - 1)
