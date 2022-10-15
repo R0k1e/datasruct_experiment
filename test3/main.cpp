dc17c03cf77b38f0035faca7329f53e01caf6bbd
昊钰 王 <wanghaoyu666@bupt.edu.cn>
2022-10-15 18:46:42 +0800

    111

--- test3/main.cpp
+++ test3/main.cpp
@@ -13,18 +13,12 @@ struct sparmattp
 using namespace std;
 void add(const sparmattp *a, const sparmattp *b, sparmattp *c);      //两矩阵相加
 void multiple(const sparmattp *a, const sparmattp *b, sparmattp *c); //两矩阵相乘
-void input(sparmattp *a);                                            //输入元素到a中
 int judge_add(const sparmattp *a, const sparmattp *b);               //判断能否相加
 int judge_multiple(const sparmattp *a, const sparmattp *b);          //判断能否相乘
+void input(sparmattp *a);                                            //输入元素到a中
 void output(const sparmattp *a);                                     //输出最后矩阵
-void transport(const sparmattp *a, sparmattp *b);                    //将a转置为b
-ofstream outfile;
-ifstream infile;
-
 int main(void)
 {
-    outfile.open("output.txt");
-    infile.open("dict.txt");
     sparmattp *a = new sparmattp;
     sparmattp *b = new sparmattp;
     sparmattp *c = new sparmattp;
@@ -32,27 +26,27 @@ int main(void)
     input(b);
     if (judge_add(a, b))
     {
-        outfile << "They can't add together" << endl;
+        cout << "They can't add together" << endl;
     }
     else
     {
         add(a, b, c);
-        outfile << "Add matrix" << endl;
+        cout << "Add matrix" << endl;
         output(c);
     }
     if (judge_multiple(a, b))
     {
-        outfile << "They can't multiple together" << endl;
+        cout << "They can't multiple together" << endl;
     }
     else
     {
         multiple(a, b, c);
-        outfile << "Multiple matrix" << endl;
+        cout << "Multiple matrix" << endl;
         output(c);
     }
 }
 
-void add(const sparmattp *a, const sparmattp *b, sparmattp *c) //两矩阵相加 未测试
+void add(const sparmattp *a, const sparmattp *b, sparmattp *c) //两矩阵相加 
 {
     c->data = new element[a->m * a->n];
     int l, m, n;
@@ -91,10 +85,8 @@ void add(const sparmattp *a, const sparmattp *b, sparmattp *c) //两矩阵相加
     c->m = a->m;
     c->n = a->n;
 }
-void multiple(const sparmattp *a, const sparmattp *b, sparmattp *c) // 两矩阵相乘 未测试
+void multiple(const sparmattp *a, const sparmattp *b, sparmattp *c) // 两矩阵相乘 
 {
-    // sparmattp *b1 = new sparmattp;
-    // transport(b,b1);
     int l, m, n, i, j;
     l = 0, m = 0, n = 0;
     int d[a->m][b->n] = {0};
@@ -122,10 +114,10 @@ void multiple(const sparmattp *a, const sparmattp *b, sparmattp *c) // 两矩阵
                 c->data[l].i = i + 1;
                 c->data[l].j = j + 1;
                 c->data[l].value = d[i][j];
-                l++;
+                l++; 
             }
         }
-    }
+    }//将非0元素输入三元组表
     c->t = l;
     c->m = a->m;
     c->n = b->n;
@@ -135,22 +127,22 @@ void input(sparmattp *a) // 输入元素到a中
     int m, n, t;
     int i, j, value;
     int count = 0;
-    outfile << "Input the row, column and non-zero number of matrix" << endl;
+    cout << "Input the row, column and non-zero number of matrix" << endl;
 
-    infile >> m >> n >> t;
+    cin >> m >> n >> t;
     a->m = m;
     a->n = n;
     a->t = t;
     a->data = new element[t];
     do
     {
-        outfile << "Input the element's row, column and its value" << endl;
-        infile >> (a->data)[count].i;
-        infile >> a->data[count].j;
-        infile >> a->data[count].value;
+        cout << "Input the element's row, column and its value" << endl;
+        cin >> (a->data)[count].i;
+        cin >> a->data[count].j;
+        cin >> a->data[count].value;
         count++;
     } while (count < t);
-    outfile << "Input finished" << endl;
+    cout << "Input finished" << endl;
 }
 int judge_add(const sparmattp *a, const sparmattp *b) //判断能否相加
 {
@@ -159,44 +151,21 @@ int judge_add(const sparmattp *a, const sparmattp *b) //判断能否相加
         flag = 1;
     if (a->n != b->n)
         flag = 1;
-    return flag;
+    return flag;//判断两矩阵行列数是否相同
 }
 int judge_multiple(const sparmattp *a, const sparmattp *b) //判断能否相乘
 {
     int flag = 0;
     if (a->n != b->m)
         flag = 1;
-    return flag;
+    return flag;//判断第一个矩阵列数是否等于第二个行数
 }
-void output(const sparmattp *a) //输出最后矩阵 未测试
+void output(const sparmattp *a) //输出最后矩阵 
 {
     for (int i = 0; i < a->t; i++)
     {
-        outfile << "row " << a->data[i].i << endl;
-        outfile << "column " << a->data[i].j << endl;
-        outfile << "value " << a->data[i].value << endl;
-    }
-}
-void transport(const sparmattp *a, sparmattp *b) //未测试
-{
-    int i, j, k;
-    b->m = a->n;
-    b->n = a->m;
-    b->t = a->t;
-    b->data = new element[b->t]();
-    int num[a->n] = {0};  //每列的非0元素个数
-    int cpot[a->n] = {0}; //每列第一个非0元素的位置
-    for (i = 0; i < a->t; i++)
-        num[a->data[i].j - 1]++; //第j列每有一个就++
-    for (i = 1; i < a->n; i++)
-        cpot[i] = cpot[i - 1] + num[i - 1];
-    for (i = 0; i < b->t; i++)
-    {
-        j = cpot[a->data[i].j - 1]; //找出a中第i个元素是第几列，并在b中找到其位置
-        cpot[a->data[i].j - 1]++;
-        b->data[j] = a->data[i];
-        k = b->data[j].i;
-        b->data[j].i = b->data[j].j;
-        b->data[j].j = k;
+        cout << "row " << a->data[i].i << endl;
+        cout << "column " << a->data[i].j << endl;
+        cout << "value " << a->data[i].value << endl;
     }
 }
